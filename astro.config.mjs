915c3057618b3352b6e2875898c1b7cb687e23ef
DerrickLeeYY <derrick.lee.yy@gmail.com>
2025-02-08 14:17:50 +0800

    Updated astro.config.mjs to fix deployment issues#

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://derrickleeyy.com",
+  site: "https://derrickleeyy.github.io/",
   base: "",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
